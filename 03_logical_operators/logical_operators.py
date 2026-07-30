efde7cef192e2c7bc84648d0f5a9f22a1d8457a3
Ruturajsinh Sarvaiya <sarvaiyaruturajsinh7575@gmail.com>
2026-07-30 15:32:05 +0530

    Update logical_operators.py

--- 03_logical_operators/logical_operators.py
+++ 03_logical_operators/logical_operators.py
@@ -6,10 +6,13 @@ and, or, and not in Python.
 print(True and True)    # True
 print(True and False)   # False
 print(False and False)  # False
+print(False and True)   # False
 
 # OR: At least one must be True  
-print(True or False)    # True
+print(True or True)    # True
 print(False or False)   # False
+print(True or False)    # True
+print(False or True)    # True
 
 # NOT: Flips the value
 print(not True)         # False
@@ -24,14 +27,13 @@ to make the output easier to understand.
 print("True and True:", True and True)
 print("True and False:", True and False)
 print("False and False:", False and False)
-
-print()
+print("False or True:", False or True)
 
 # OR: At least one condition must be True
-print("True or False:", True or False)
+print("True or True:", True or True)
 print("False or False:", False or False)
-
-print()
+print("True or False:", True or False)
+print("False or True:", False or True)
 
 # NOT: Reverses the Boolean value
 print("not True:", not True)
