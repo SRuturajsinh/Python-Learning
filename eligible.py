f683436e35e3f6cbc1be99f35c933c0efaf5cc28
Ruturajsinh Sarvaiya <sarvaiyaruturajsinh7575@gmail.com>
2026-08-06 17:55:23 +0530

    Add files via upload

---
+++ eligible.py
@@ -0,0 +1,61 @@
+"""
+Pattern Printing in Python
+
+This program demonstrates different star patterns
+using both for loops and while loops.
+"""
+#--------------------------------
+# Pattern 1 - Increasing Triangle
+#--------------------------------
+for i in range(1,6):
+        print("*"*i)
+
+i=1
+while i<6:
+        print("*"*i)
+        i+=1
+# Output
+# *
+# **
+# ***
+# ****
+# *****
+
+#--------------------------------
+# Pattern 2 - Decreasing Triangle
+#--------------------------------
+for i in range(5,0,-1):
+        print("*"*i)
+
+i=5
+while i>0:
+     print("*"*i)
+     i-=1
+# Output
+# *****
+# ****
+# ***
+# **
+# *
+
+
+#-----------------------------------
+# Pattern 3 - Right-Aligned Triangle
+#-----------------------------------
+i=5
+for j in range(1,6):
+    i-=1
+    print(" "*i + "*"*j)
+
+i=5
+j=1
+while j<6:
+       i-=1
+       print(" "*i + "*"*j)
+       j+=1
+# Output
+#     *
+#    **
+#   ***
+#  **** 
+# *****
