de3ccfdcf44dcd8a0d542fc2e548a582d81fbb71
Ruturajsinh Sarvaiya <sarvaiyaruturajsinh7575@gmail.com>
2026-08-11 16:38:07 +0530

    Add docstring and improve digit counting code
    Added a docstring explaining the purpose of the code. The code counts the number of digits in a number using a function, while loop, and for loop.

--- Challenges/challenge_05_digit.py
+++ Challenges/challenge_05_digit.py
@@ -1,8 +1,14 @@
+"""
+Count the number of digits in a number
+using function, while loop and for loop.
+"""
 
+# Using Function
 num=int(input("Enter a Number: "))
 lenght=len(str(num))
 print(f"Number of digits by function: {lenght}")
 
+# Using while loop
 count=0
 while num>0:
     num=num//10
