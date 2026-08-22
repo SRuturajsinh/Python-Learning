2f53b4e6e596cb5ac5dbeea9e23e8756e5b02786
Ruturajsinh Sarvaiya <sarvaiyaruturajsinh7575@gmail.com>
2026-08-22 15:06:53 +0530

    Add files via upload

---
+++ today.py
@@ -0,0 +1,30 @@
+"""
+list comprehension.
+
+You've already covered enough basic list operations. 
+This is the next important step before moving on to tuples/sets/dictionaries.
+"""
+
+number = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# long method using foor loop
+sq = []
+for i in range(len(number)):
+    sq.append(number[i]*number[i])
+print("Square number using method1:",sq)
+
+sq1 = []
+for number in number:
+    sq1.append(number*number)
+print("Square number using method2:",sq1)
+
+
+# list comprehension
+square = [number * number for number in number]
+print("Square number using list comprehension:",square)
+
+
+# list comprehension with condition
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+even_nums = [nums for nums in nums if nums%2==0]
+print("Even numbers:",even_nums)
