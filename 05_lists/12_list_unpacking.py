e12148b74e347226184f840d571aba04995eb9f2
SRuturajsinh <sarvaiyaruturajsinh7775@gmail.com>
2026-09-12 16:54:05 +0530

    Update 12_list_unpacking.py

--- 05_lists/12_list_unpacking.py
+++ 05_lists/12_list_unpacking.py
@@ -29,3 +29,41 @@ a, b = nums        # Error
 print(a)  # 1
 print(b)  # 2
 print(c)  # 3
+
+
+# ( * ) lets you collect multiple values
+
+num = [20,30,50,60,43,75] 
+
+*a, b , c =num
+
+print(a) # a = [20,30,50,60]
+print(b) # b = [43]
+print(c) # c = [75]
+
+"""
+This will allow you to assign multiple elements as a list to the single veriable
+"""
+
+number = [2,4,3,1,5,6]
+
+*a, rest = number
+
+print(a)   # [2,4,3,1,5]
+print(rest)# [6] another remaining elemnts 
+
+# more examples
+
+numbers = [2,4,5,2,1,6,4,6]
+
+a, *b , c = numbers
+print(a) # [2]
+print(b) # [4,5,2,1,6,4]
+print(c) # [6] 
+
+numbers = [2,4,5,2,1,6,4,6]
+
+a,b,*c = numbers
+print(a) # [2]
+print(b) # [4]
+print(c) # [5,2,1,6,4,6]
