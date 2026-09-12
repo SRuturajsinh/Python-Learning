ff1c67a06c9b5e36d721b368aa31dac70641bd42
SRuturajsinh <sarvaiyaruturajsinh7575@gmail.com>
2026-09-12 21:06:00 +0530

    Create 01_dictionary_basic.py

---
+++ 06_dictionaries/01_dictionary_basic.py
@@ -0,0 +1,21 @@
+"""
+Dictionary Introduction
+
+A dictionary stores data in key-value pairs.
+
+key   -> value
+"""
+
+student = {
+    "name": "Ruturaj",
+    "age": 20,
+    "course": "Python"
+}
+
+print(student)
+
+# Accessing values using keys
+
+print(student["name"])
+print(student["age"])
+print(student["course"])
