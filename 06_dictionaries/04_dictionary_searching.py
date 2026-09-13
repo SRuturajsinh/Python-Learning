bca537e1070da08a4f4426c86eca15467fb0a84f
SRuturajsinh <sarvaiyaruturajsinh7575@gmail.com>
2026-09-13 14:59:43 +0530

    Create 04_dictionary_searching.py

---
+++ 06_dictionaries/04_dictionary_searching.py
@@ -0,0 +1,38 @@
+"""
+Dictionary Searching
+
+We can check whether a key or value
+exists in a dictionary.
+"""
+
+student = {
+    "name": "Ruturaj",
+    "age": 20,
+    "course": "Python"
+}
+
+
+# Checking if a key exists
+
+print("name" in student)
+print("city" in student)
+
+
+# Checking if a key does NOT exist
+
+print("city" not in student)
+
+
+# Checking if a value exists
+
+print("Ruturaj" in student.values())
+print("Java" in student.values())
+
+
+# Using if
+
+if "course" in student:
+    print("Course exists")
+
+if "city" not in student:
+    print("City does not exist")
